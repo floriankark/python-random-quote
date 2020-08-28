d9e4fd4b87626c7a38e5a57aea05b2ab43f2c713
Florian Kark <floriankark@Flo-MacBook-Pro.fritz.box>
2020-08-28 16:50:35 +0200

    printing out random number of quotes after the random first one

--- get-quote.py
+++ get-quote.py
@@ -8,8 +8,10 @@ def prime():
   f.close()
   last = len(quotes) - 1
   rnd = random.randint(0,last)
-
-  print(quotes[rnd])
+  mltpl = random.randint(0,last-rnd)
+  while mltpl >= 0:
+      print(quotes[rnd + mltpl])
+      mltpl -= 1
 
 if __name__== "__main__":
   prime()
