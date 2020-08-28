3fd30cc1fa02dbccee932b93e85d84182d60ef32
floriankark <58469871+floriankark@users.noreply.github.com>
2020-08-28 16:53:00 +0200

    Update get-quote.py

--- get-quote.py
+++ get-quote.py
@@ -7,17 +7,11 @@ def prime():
   quotes = f.readlines()
   f.close()
   last = len(quotes) - 1
-<<<<<<< HEAD
   rnd = random.randint(0,last)
   mltpl = random.randint(0,last-rnd)
   while mltpl >= 0:
       print(quotes[rnd + mltpl])
       mltpl -= 1
-=======
-  rnd = random.randint(1,13)
-
-  print(quotes[rnd])
->>>>>>> 844fd353669858bcf4facd1b6560e99e101d4647
 
 if __name__== "__main__":
   prime()
