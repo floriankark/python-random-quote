a7a9e5a69851f2860d10ee3dde62d42047547728
Florian Kark <floriankark@Flo-MacBook-Pro.fritz.box>
2020-08-28 16:57:51 +0200

    removing newline by adding .rstrip()

--- get-quote.py
+++ get-quote.py
@@ -10,7 +10,7 @@ def prime():
   rnd = random.randint(0,last)
   mltpl = random.randint(0,last-rnd)
   while mltpl >= 0:
-      print(quotes[rnd + mltpl])
+      print(quotes[rnd + mltpl].rstrip())
       mltpl -= 1
 
 if __name__== "__main__":
